81bc5a86b2e93b2fe7d6b1897f9ab57931beb883
Julian Castro <jcastro@artech-consulting.com>
2022-05-02 17:37:00 -0300

    proyecto base

---
+++ src/database.php
@@ -0,0 +1,37 @@
+    <?php
+
+	function conectarAMySQL()
+	{
+		$servername = "localhost";
+		$database = "db_pokedex";
+		$username = "root";
+		$password = "root";
+		// Create connection
+		$conn = mysqli_connect($servername, $username, $password, $database);
+		// Check connection
+		if (!$conn) {
+			die("Connection failed: " . mysqli_connect_error());
+		}
+		echo "Connected successfully";
+
+
+		$sql = "SELECT * FROM pokemon";
+
+		$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) {
+			// output data of each row
+			while ($row = $result->fetch_assoc()) {
+				echo "id: " . $row["id"] . " - Name: " . $row["nombre"];
+			}
+		} else {
+			echo "0 results";
+		}
+		$conn->close();
+	}
+
+
+
+
+	?>
+
