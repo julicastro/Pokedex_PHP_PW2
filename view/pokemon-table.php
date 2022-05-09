7cfa98cedccb1c00568b09bd340e335bb78e5cb9
Len <sublea1999@gmail.com>
2022-05-08 21:18:13 -0300

    pequeños edits de color

--- view/pokemon-table.php
+++ view/pokemon-table.php
@@ -11,34 +11,36 @@ include_once("config/Configuration.php");
 
 <div class='w3-container w3-content w3-center w3-padding-64' style='max-width:800px' id='band'>
     <h2 class='w3-wide text-center'>Pokemones</h2>
-    <table class='w3-table pokemon-table'>
+    <table class="table table-striped table-hover">
+        <thead>
         <tr>
-            <th>ID</th>
-            <th>Imagen</th>
-            <th>Nombre</th>
-            <th>Tipo</th>
-            <th>Numero</th>
+            <th scope="col">ID</th>
+            <th scope="col">Imagen</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Tipo</th>
+            <th scope="col">Numero</th>
         </tr>
-
-
+        </thead>
+        <tbody>
         <?php
         //Hay q especificar bien los atrivutos
         //Esto seria CON base de datos
         //=======HARCODEADO===========
-        $conn = mysqli_connect("localhost", "root", "root", "db_pokedex");
+        $conn = mysqli_connect("localhost", "root", "", "db_pokedex");
         $sql = "SELECT * FROM pokemon";
         $result = $conn->query($sql);
 
 
 
         while ($row = $result->fetch_assoc()) {
-            echo '<tr>
-                                <td>' . $row['id'] . '</td>
-                                <td>' . $row['imagen'] . '</td>
-                                <td>' . $row['nombre'] . '</td>
-                                <td>' . $row['tipo'] . '</td>
-                                <td>' . $row['numero'] . '</td>
-                            </tr>';
+            echo '<tr style="text-align-last: center;">
+            <th scope="row">' . $row['id'] . '</th>
+            <td>' . $row['imagen'] . '</td>
+            <td>' . $row['nombre'] . '</td>
+            <td>' . $row['tipo'] . '</td>
+            <td>' . $row['numero'] . '</td>
+           
+                </tr>';
         }
         $conn->close();
         ?>
