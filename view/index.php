af6593dc719f403de9cb15b6a550a4aa4fcb4ad6
Jannett Arami Aquino Romero <jaquinoromero@alumno.unlam.edu.ar>
2022-05-17 22:22:45 -0300

    edicion

--- view/index.php
+++ view/index.php
@@ -13,13 +13,18 @@ require_once '../templates/buscador.php';
 <?php
 showHead();
 ?>
-
-
+<body style="background: #d2c575;font-family: cursive;" style="text-align: -webkit-center;">
+    <div class="container-fluid">
+        <form action="user.php" class="d-flex" method="post" enctype="multipart-form-data">
+        <input class="form-control me-2" type="text" placeholder="Ingrese su nombre de usuario"  name="usuario">
+        <input class="form-control me-2" type="text" placeholder="Ingrese su contraseña"  name="password">
+        <button class="btn btn-outline-success" type="submit">Ingresar</button>
+        </form>
+    </div>
+</boby>
 <body style="background: #d2c575;font-family: cursive;" style="text-align: -webkit-center;">
 
-	<?php
-	showNav("Lista de Pokemones");
-	?>
+
 	<?php
 	showSeach();
 	?>
